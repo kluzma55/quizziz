c6abcca4db4a92c63e120d77886cae806d9eb2a1
kluzma55 <kluzma55@osu.cz>
2025-04-14 19:44:59 +0200

    Základní spuštění aplikace

---
+++ src/main/java/com/example/quizziz/controller/QuizzizController.java
@@ -0,0 +1,9 @@
+package com.example.quizziz.controller;
+
+import lombok.Value;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class QuizzizController {
+    @Value("#")
+}
---
+++ src/main/java/com/example/quizziz/model/entity/QuizResult.java
@@ -0,0 +1,4 @@
+package com.example.quizziz.model.entity;
+
+public class QuizResult {
+}
