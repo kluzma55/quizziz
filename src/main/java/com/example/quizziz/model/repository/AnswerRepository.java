efe5fc77a1325bef1a438874b2575e5b1122eb29
Rebeka Motyčáková <rebeka@MacBook-Air-uzivatela-Rebeka.local>
2025-04-26 00:10:52 +0200

    Uprava databaze a veci suvisiacich

--- src/main/java/com/example/quizziz/model/repository/AnswerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.quizziz.model.repository;
-
-import com.example.quizziz.model.entity.Answer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AnswerRepository extends JpaRepository<Answer, Integer> {
-}
